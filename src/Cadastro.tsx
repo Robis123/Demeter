189dceb1497bb074ff16a4219ecaeddf6117c8fe
unknown <diogobites@gmail.com>
2023-11-02 22:52:41 -0300

    arrumando bugs do teclado

--- src/Cadastro.tsx
+++ src/Cadastro.tsx
@@ -2,11 +2,13 @@ import UserContext from "./UserContext";
 import AuthContext from "./AuthContext";
 import { StyleSheet, View, Image, Text } from "react-native";
 import { Color, Border, FontFamily, FontSize, Padding } from "../GlobalStyles";
-import {app, db, getFirestore, collection, addDoc} from "./firebase/firebase.js"
+import { app, db, getFirestore, collection, addDoc } from "./firebase/firebase.js"
 import React from "react";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { Button, TextInput } from "react-native-paper";
+import { KeyboardAvoidingView, Platform, Keyboard } from 'react-native';
+
 import {
   GoogleSignin,
   GoogleSigninButton,
@@ -24,20 +26,20 @@ export default function Cadastro() {
 
   // Essa função aqui que vai adicionar os elementos em uma coleção do banco de dados, no caso dessa, produtores
   const addProdutor = async () => {
-  
-          try {
-            const docRef = await addDoc(collection(db, "produtores"), {
-              nome: nome,
-              cpf: cpf,
-              cnpj: cnpj,
-              telefone: telefone,
-              email: email
-            });
 
-            console.log("Document written with ID: ", docRef.id);
-          } catch (e) {
-            console.error("Error adding document: ", e);
-}
+    try {
+      const docRef = await addDoc(collection(db, "produtores"), {
+        nome: nome,
+        cpf: cpf,
+        cnpj: cnpj,
+        telefone: telefone,
+        email: email
+      });
+
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
   }
 
   const { signOut } = React.useContext(AuthContext);
@@ -53,49 +55,81 @@ export default function Cadastro() {
   }, [register]);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Cadastro de dados</Text>
-      <TextInput
-        style={styles.input}
-        label="Nome"
-        // Tem que colocar o value aqui 
-        value={nome}
-        onSubmitEditing={addProdutor}
-        onChangeText={(text) => setNome(text)}
-      />
-      <TextInput
-        style={styles.input}
-        label="CPF/CNPJ"
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      enabled
+      keyboardVerticalOffset={-254}
+    >
+      <View style={[styles.container, styles.innerContainer]}>
+        <Text style={styles.text}>Cadastro de dados</Text>
+        <TextInput
+          style={styles.input}
+          label="Nome"
+          value={nome}
+          onSubmitEditing={addProdutor}
+          onChangeText={(text) => {
+            // Limita o texto a 50 caracteres
+            if (text.length <= 50) {
+              setNome(text);
+            }
+          }}
+          maxLength={50} // Define o limite de caracteres
+        />
+        <TextInput
+          style={styles.input}
+          label="CPF/CNPJ"
         // Não coloquei aqui pq tem q resolver ainda se vai ser cpf e cnpj e um campo só (usando máscaras), 
         // ou separados
-      />
-      <TextInput
-        style={styles.input}
-        label="Numero"
-        value={formatPhoneNumber(telefone)}
-        onSubmitEditing={addProdutor}
-        onChangeText={(text) => setTelefone(text)}
-        keyboardType="numeric" 
-      />
-      <TextInput
-        style={styles.input}
-        label="Email"
-        value={email}
-        onSubmitEditing={addProdutor}
-        onChangeText={(text) => setEmail(text)}
-      />
-      <Button
-        style={styles.button}
-        mode="contained"
-        onPress={addProdutor}
-      >
-        Cadastrar
-      </Button>
-      <Button style={styles.button} onPress={signOut}>
-        Sign Out
-      </Button>
-    </View>
+        />
+        <TextInput
+          style={styles.input}
+          label="Numero"
+          value={formatPhoneNumber(telefone)}
+          onSubmitEditing={addProdutor}
+          onChangeText={(text) => setTelefone(text)}
+          keyboardType="numeric"
+        />
+        <TextInput
+          style={styles.input}
+          label="Email"
+          value={email}
+          onSubmitEditing={addProdutor}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Button
+          style={styles.button}
+          mode="contained"
+          onPress={addProdutor}
+        >
+          Cadastrar
+        </Button>
+        <Button style={styles.button} onPress={signOut}>
+          Sign Out
+        </Button>
+      </View>
+    </KeyboardAvoidingView>
   );
+}
+function keyboard() {
+  const [keyboardHeight, setKeyboardHeight] = useState(0);
+
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      'keyboardDidShow',
+      (event) => {
+        // Obtém a altura do teclado do evento
+        setKeyboardHeight(event.endCoordinates.height);
+      }
+    );
+
+    // Remove o listener quando o componente é desmontado
+    return () => {
+      keyboardDidShowListener.remove();
+    };
+  }, []);
+  console.log(keyboardHeight * (-1))
+  return keyboardHeight; // Retorna a altura do teclado
 }
 
 function formatPhoneNumber(value) {
@@ -120,6 +154,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     padding: 16,
   },
+  innerContainer: {
+    top: 140,
+    width: '100%', // Ocupa toda a largura do dispositivo
+    paddingHorizontal: 5, // Adiciona um espaçamento nas laterais
+    alignItems: 'center',
+  },
   image: {
     marginBottom: 20,
     borderRadius: 100,
@@ -134,7 +174,7 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   button: {
-    width: 150,
+    width: '100%',
     height: 65,
     marginBottom: 250,
   },
