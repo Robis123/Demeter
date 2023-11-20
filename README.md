cef107987aae241b19258bd22a1decc5040542f4
diogo.bites <diogo.bites@iti.gov.br>
2023-11-20 19:10:01 -0300

    :floppy_disk: primeiro commit back

--- README.md
+++
@@ -1,2 +0,0 @@
-# Demeter
-Projeto realizado para o TCC do curso de Ciência da computação no Centro Universitário de Brasília - CEUB.
--- src/tabs/explorar.tsx
+++
@@ -1,9 +0,0 @@
-import { VStack, Text } from "native-base";
-import React from "react";
-export default function Explorar() {
-  return (
-    <VStack>
-      <Text>Tela Explorar</Text>
-    </VStack>
-  );
-}
